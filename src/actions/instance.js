2f976790f3da49f8b1139af3100f05065d72f0ec
yeremiadio <raikkonendio@gmail.com>
2021-08-08 22:53:16 +0700

    instance clean codes

--- src/actions/instance.js
+++ src/actions/instance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { token } from "../config/token";
-// import Cookies from "js-cookie";
-import { logOut } from "./auth/authAction";
 
 const instance = axios.create({
   baseURL: "http://127.0.0.1:8000",
@@ -10,34 +8,5 @@ const instance = axios.create({
     Authorization: `Bearer ${token()}`,
   },
 });
-
-// instance.interceptors.request.use(
-//   (config) => {
-//     if (token()) {
-//       config.headers.Authorization = token() ? `Bearer ${token()}` : "";
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     Promise.reject(error);
-//   }
-// );
-
-// instance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error) {
-//       console.log(error)
-//       logOut();
-//       // Cookies.remove("access");
-//       setTimeout(() => {
-//         window.location.reload();
-//       }, 2000);
-//       return Promise.reject(error);
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
 
 export default instance;
